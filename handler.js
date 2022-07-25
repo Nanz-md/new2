097a5ad7223ba95b3257f36498a762ccfc242c47
Nanz-md <108403508+Nanz-md@users.noreply.github.com>
2022-07-25 15:02:26 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -273,9 +273,12 @@ module.exports = {
                     if (!isNumber(user.lastnebang)) user.lastnebang = 0
                     if (!isNumber(user.lastberkebon))user.lastberkebon = 0
                     if (!isNumber(user.lastadventure)) user.lastadventure = 0
-                    if (!('autorestart' in settings)) settings.autorestart = true
-                    if (!('restartDB' in settings)) settings.restartDB = 0
-                } else global.db.data.settings[this.user.jid] = {
+                    if (!('autorestart' in settings)) 
+settings.autorestart = true
+                    if (!('restartDB' in settings))
+settings.restartDB = 0
+                } else global.db.data.
+settings[this.user.jid] = {
                     autorestart: true,
                     restartDB: 0
                 } else global.db.data.users[m.sender] = {
